a41fb9a039b204bbadf658c82b75ebc615f7e67f
Abdelghani Braimi <abraimi@student.1337.ma>
2026-04-02 16:44:47 +0100

    First commit

--- src/plugins/backgrounds/wallhaven/api.ts
+++ src/plugins/backgrounds/wallhaven/api.ts
@@ -6,17 +6,34 @@ import { WallhavenFetchOptions } from "./types";
  * limited to what you can request.
  * So we need to validate the params for what can be
  * accessed with an api key and what cannot.
- *
+ * And apparently only NSFW that requires an api key.
  * @param WallhavenFetchOptions
  */
 
 export const fetchWallhavenImages = async ({
   categories,
+  atleast,
+  colors,
+  order,
+  page,
+  purity,
+  ratios,
+  resolutions,
+  seed,
+  sorting,
+  topRange,
 }: WallhavenFetchOptions) => {
   const url = "https://wallhaven.cc/api/v1/search";
   const params = new URLSearchParams();
 
-  if (!WALLHAVEN_API_KEY) {
-    // TODO: Verify the params for what requires an api key
+  if (!WALLHAVEN_API_KEY && purity.nsfw) {
+    throw new Error("You must set WALLHAVEN_API_KEY to access nsfw");
   }
+  // TODO: we have to switch from function params to api request url params 1 by 1.
+  params.set(
+    "categories",
+    (categories.anime ? "1" : "0") +
+      (categories.general ? "1" : "0") +
+      (categories.people ? "1" : "0"),
+  );
 };
--- src/plugins/backgrounds/wallhaven/types.ts
+++ src/plugins/backgrounds/wallhaven/types.ts
@@ -29,11 +29,12 @@ type TopRange = "1d" | "3d" | "1w" | "1M" | "3M" | "6M" | "1y";
 
 type WidthXheight = `${number}x${number}`;
 
+type Colors = `#${string}`;
 /*
   The seed requires the following requirements: [a-zA-Z0-9]{6}
   And it's used to get random results.
 */
-export interface WallhavenFetchOptions {
+interface WallhavenFetchOptions {
   categories: Categories;
   purity: Purity;
   sorting: Sorting;
@@ -42,7 +43,60 @@ export interface WallhavenFetchOptions {
   atleast: WidthXheight;
   resolutions: WidthXheight[];
   ratios: WidthXheight[];
-  colors: string[];
+  colors: Colors[];
   page: number;
   seed: string;
 }
+
+const defaultData: WallhavenFetchOptions = {
+  categories: {
+    general: true,
+    anime: true,
+    people: true,
+  },
+  purity: {
+    sfw: true,
+    sketchy: false,
+    nsfw: false,
+  },
+  sorting: "date_added",
+  order: "desc",
+  topRange: "1M",
+  atleast: "1920x1080",
+  resolutions: ["1920x1080", "2560x1440"],
+  ratios: ["16x9"],
+  colors: [],
+  page: 1,
+  seed: "",
+};
+
+interface ReturnTypes {
+  id: string;
+  url: string;
+  short_url: string;
+  views: number;
+  favorites: number;
+  source: string;
+  purity: string;
+  category: string;
+  dimension_x: number;
+  dimension_y: number;
+  resolution: string;
+  ratio: string;
+  file_size: number;
+  file_type: string;
+  created_at: string;
+  colors: Colors[];
+  path: string;
+  thumbs: {
+    large: string;
+    original: string;
+    small: string;
+  };
+}
+
+export {
+  type WallhavenFetchOptions,
+  type ReturnTypes,
+  defaultData as wallhavenDefaultData,
+};
